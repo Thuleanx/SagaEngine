485dce2ee916687de503fcdbbedf901a3a6fc1cc
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2023-03-17 19:57:06 -0400

    event loading + playing. fmod simple project

--- Engine/Audio/audioEngine.cpp
+++ Engine/Audio/audioEngine.cpp
@@ -6,11 +6,11 @@ namespace Saga::AudioEngine {
 	AudioImplementation implementation = AudioImplementation();
 
 	namespace {
-		void ensureFMOD_OK(FMOD_RESULT result, const std::string& failMessage, ...) {
+        void ensureFMOD_OK(FMOD_RESULT result, const char* failMessage, ...) {
 			if (result != FMOD_RESULT::FMOD_OK) {
 				va_list arg_ptr;
 				va_start(arg_ptr, failMessage);
-				SERROR(("FMOD Error: %s. With mesage: " + failMessage).c_str(), result, arg_ptr);
+                SERROR(("FMOD Error: %s. With mesage: " + std::string(failMessage)).c_str(), result, arg_ptr);
 				va_end(arg_ptr);
 			}
 		}
@@ -30,45 +30,103 @@ namespace Saga::AudioEngine {
 
 	bool release() {
 		ensureFMOD_OK(FMOD_Studio_System_Release(implementation.studioSystem), "Failed to release fmod system.");
-		SINFO("Fmod Studio System Released.");
-		delete implementation.studioSystem;
+        SINFO("Fmod Studio System Released.");
 		return true;
 	}
 
-	void loadBank(const std::string& strBankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags) {
-		if (implementation.banks.count(strBankName)) {
-			SWARN("Tried to load already loaded fmod bank %s. Does nothing.", strBankName.c_str());
+	void loadBank(const std::string& bankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags) {
+		if (implementation.banks.count(bankName)) {
+			SWARN("Tried to load already loaded fmod bank %s. Does nothing.", bankName.c_str());
 			return;
 		}
 		FMOD_STUDIO_BANK* bank;
-		ensureFMOD_OK(FMOD_Studio_System_LoadBankFile(implementation.studioSystem, strBankName.c_str(), flags, &bank), "Can't load fmod bank \"%s\". Ensure the provided name is correct.", strBankName.c_str());
-		if (bank) implementation.banks[strBankName] = bank;
+		ensureFMOD_OK(FMOD_Studio_System_LoadBankFile(implementation.studioSystem, bankName.c_str(), flags, &bank), "Can't load fmod bank \"%s\". Ensure the provided name is correct.", bankName.c_str());
+		if (bank) implementation.banks[bankName] = bank;
 	}
 
-	void unloadBank(const std::string& strBankName) {
-		if (!implementation.banks.count(strBankName)) {
-			SWARN("Tried to unload not loaded fmod bank %s. Please ensure correct bank name.", strBankName.c_str());
+	void unloadBank(const std::string& bankName) {
+		if (!implementation.banks.count(bankName)) {
+			SWARN("Tried to unload not loaded fmod bank %s. Please ensure correct bank name.", bankName.c_str());
 			return;
 		}
-		ensureFMOD_OK(FMOD_Studio_Bank_Unload(implementation.banks[strBankName]), "Can't unload fmod bank \"%s\". Ensure the provided name is correct.", strBankName.c_str());
-		implementation.banks.erase(strBankName);
+		ensureFMOD_OK(FMOD_Studio_Bank_Unload(implementation.banks[bankName]), "Can't unload fmod bank \"%s\". Ensure the provided name is correct.", bankName.c_str());
+		implementation.banks.erase(bankName);
 	}
 
-	void loadEvent(const std::string& strEventName) { }
+	void loadEvent(const std::string& eventName) {
+		if (implementation.events.count(eventName)) {
+			SWARN("Tried to load already loaded fmod event %s. Please ensure the correct event name.", eventName.c_str());
+			return;
+		}
+		FMOD_STUDIO_EVENTDESCRIPTION *description = NULL;
+		ensureFMOD_OK(FMOD_Studio_System_GetEvent(implementation.studioSystem, eventName.c_str(), &description), "Failed to load event: %s.", eventName.c_str());
+		implementation.events[eventName] = description;
+	}
+
+	FMOD_STUDIO_EVENTINSTANCE* playEvent(const std::string& eventName) {
+		if (!implementation.events.count(eventName)) {
+			SWARN("Event sample data for fmod event \"%s\" has not been loaded. Please load the event first!", eventName.c_str());
+            return NULL;
+		}
+		FMOD_STUDIO_EVENTINSTANCE *instance = NULL;
+		playEvent(instance);
+		return instance;
+	}
+
+	void playEvent(FMOD_STUDIO_EVENTINSTANCE* instance) {
+		ensureFMOD_OK(FMOD_Studio_EventInstance_Start(instance), "Failed to start fmod event.");
+	}
+
+    void stopEvent(FMOD_STUDIO_EVENTINSTANCE* event, bool immediate) {
+		if (!event) {
+			SERROR("Tried to stop a null event.");
+			return;
+		}
+
+		FMOD_STUDIO_STOP_MODE mode = immediate ? FMOD_STUDIO_STOP_IMMEDIATE : FMOD_STUDIO_STOP_ALLOWFADEOUT;
+		ensureFMOD_OK(FMOD_Studio_EventInstance_Stop(event, mode), "Cannot stop an fmod event.");
+	}
+
+	void releaseEvent(FMOD_STUDIO_EVENTINSTANCE* event) {
+		if (!event) {
+			SERROR("Tried to release a null event.");
+			return;
+		}
+		ensureFMOD_OK(FMOD_Studio_EventInstance_Release(event), "Cannot release an fmod event.");
+	}
+
+	void releaseAllEventInstances(const std::string& eventName) {
+		if (!implementation.events.count(eventName)) {
+			SWARN("Tried to release instances of event %s that has not been loaded.", eventName.c_str());
+			return;
+		}
+		ensureFMOD_OK(FMOD_Studio_EventDescription_ReleaseAllInstances(implementation.events[eventName]), "Fail to release instance for event \"%s\".", eventName.c_str());
+	}
+
+	void unloadEvent(const std::string& eventName) {
+		if (!implementation.events.count(eventName)) {
+			SWARN("Tried to unload event %s that has not been loaded.", eventName.c_str());
+			return;
+		}
+		// this doesn't neccessarily unload until all instances of the event is released, so beware
+		ensureFMOD_OK(FMOD_Studio_EventDescription_UnloadSampleData(implementation.events[eventName]), "Fail to unload sample data for event \"%s\".", eventName.c_str());
+		implementation.events.erase(eventName);
+	}
+
+
 	void loadsound(const std::string& strSoundName, bool b3d, bool bLooping, bool bStream) { }
 	void unLoadSound(const std::string& strSoundName) { }
 	void set3dListenerAndOrientation(const glm::vec3& vPos, float fVolumedB) { }
 	void playSound(const std::string& strSoundName, const glm::vec3& vPos, float fVolumedB) { }
-	void playEvent(const std::string& strEventName) { }
 	void stopChannel(int nChannelId) { }
-	void stopEvent(const std::string& strEventName, bool bImmediate) { }
-	void geteventParameter(const std::string& strEventName, const std::string& strEventParameter, float* parameter) { }
-	void setEventParameter(const std::string& strEventName, const std::string& strParameterName, float fValue) { }
+	void stopEvent(const std::string& eventName, bool bImmediate) { }
+	void geteventParameter(const std::string& eventName, const std::string& strEventParameter, float* parameter) { }
+	void setEventParameter(const std::string& eventName, const std::string& strParameterName, float fValue) { }
 	void stopAllChannels() { }
 	void setChannel3dPosition(int nChannelId, const glm::vec3& vPosition) { }
 	void setChannelvolume(int nChannelId, float fVolumedB) { }
     bool isPlaying(int nChannelId) { return false; }
-    bool isEventPlaying(const std::string& strEventName) { return false; }
+    bool isEventPlaying(const std::string& eventName) { return false; }
     float dbToVolume(float db) { return 0; }
     float volumeTodb(float volume) { return 0; }
     FMOD_VECTOR vectorToFmod(const glm::vec3& vPosition) { return FMOD_VECTOR{vPosition.x, vPosition.y, vPosition.z}; }
--- Engine/Audio/audioEngine.h
+++ Engine/Audio/audioEngine.h
@@ -12,7 +12,7 @@ namespace Saga {
 
 		typedef std::map<std::string, FMOD_SOUND*> SoundMap;
 		typedef std::map<int, FMOD_CHANNEL*> ChannelMap;
-		typedef std::map<std::string, FMOD_STUDIO_EVENTINSTANCE *> EventMap;
+		typedef std::map<std::string, FMOD_STUDIO_EVENTDESCRIPTION *> EventMap;
 		typedef std::map<std::string, FMOD_STUDIO_BANK*> BankMap;
 
 		BankMap banks;
@@ -24,30 +24,41 @@ namespace Saga {
 	namespace AudioEngine {
 		extern AudioImplementation implementation;
 
-		const int maxChannels = 2;
+		const int maxChannels = 32;
 
 		bool init();
 		void update();
 		bool release();
 
-		void loadBank(const std::string& strBankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags = FMOD_STUDIO_LOAD_BANK_NORMAL);
-		void loadEvent(const std::string& strEventName);
+		// banks
+		void loadBank(const std::string& bankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags = FMOD_STUDIO_LOAD_BANK_NORMAL);
+		void unloadBank(const std::string& bankName);
+
+		// events
+		void loadEvent(const std::string& eventName);
+		FMOD_STUDIO_EVENTINSTANCE* playEvent(const std::string& eventName);
+		void playEvent(FMOD_STUDIO_EVENTINSTANCE* event);
+		void stopEvent(FMOD_STUDIO_EVENTINSTANCE* event, bool immediate = false);
+		void releaseEvent(FMOD_STUDIO_EVENTINSTANCE* event);
+		void releaseAllEventInstances(const std::string& eventName);
+		void unloadEvent(const std::string& eventName);
+
+		// sound
 		void loadsound(const std::string& strSoundName, bool b3d = true, bool bLooping = false, bool bStream = false);
 		void unLoadSound(const std::string& strSoundName);
-		void set3dListenerAndOrientation(const glm::vec3& vPos = glm::vec3{ 0, 0, 0 }, float fVolumedB = 0.0f);
 		void playSound(const std::string& strSoundName, const glm::vec3& vPos = glm::vec3{ 0, 0, 0 }, float fVolumedB = 0.0f);
-		void playEvent(const std::string& strEventName);
 		void stopChannel(int nChannelId);
-		void stopEvent(const std::string& strEventName, bool bImmediate = false);
-		void geteventParameter(const std::string& strEventName, const std::string& strEventParameter, float* parameter);
-		void setEventParameter(const std::string& strEventName, const std::string& strParameterName, float fValue);
+		void geteventParameter(const std::string& eventName, const std::string& strEventParameter, float* parameter);
+		void setEventParameter(const std::string& eventName, const std::string& strParameterName, float fValue);
 		void stopAllChannels();
 		void setChannel3dPosition(int nChannelId, const glm::vec3& vPosition);
 		void setChannelvolume(int nChannelId, float fVolumedB);
 		bool isPlaying(int nChannelId);
-		bool isEventPlaying(const std::string& strEventName);
+		bool isEventPlaying(const std::string& eventName);
 		float dbToVolume(float db);
 		float volumeTodb(float volume);
+
+		void set3dListenerAndOrientation(const glm::vec3& vPos = glm::vec3{ 0, 0, 0 }, float fVolumedB = 0.0f);
 		FMOD_VECTOR vectorToFmod(const glm::vec3& vPosition);
 	}
 };
